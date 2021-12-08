7d781c3f7cb637e194f8579e32ee40b5203152f8
Qi Zhang <zq170829@gmail.com>
2021-12-07 22:56:23 -0600

    final

--- src/components/MyComponent.test.js
+++ src/components/MyComponent.test.js
@@ -6,4 +6,19 @@ describe("MyComponent", () => {
   it("should render correctly", () => {
     const component = shallow(<MyComponent />);
   });
+  it("should render initial layout", () => {
+    const component = shallow(<MyComponent />);
+    expect(component.getElements()).toMatchSnapshot();
+  });
+  it("should create an entry in component state", ()=> {
+    const component = shallow(<MyComponent/>);
+    const form = component.find('input');
+    //when
+    form.props().onChange({target: {
+      name:'myName',
+      value:'myValue'
+    }});
+    //then
+    expect(component.state('input')).toBeDefined();
+  })
 });
